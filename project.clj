526e443767f5a2ec60d3c399ddafbe2ace21f1cb
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-11-06 11:22:13 +0900

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hozumi/clj-glob "0.1.0"
+(defproject org.clojars.hozumi/clj-glob "0.1.1"
   :description "Quickly locate files with glob patterns"
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]])
